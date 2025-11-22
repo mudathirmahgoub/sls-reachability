cb0fe4826778c062cdb50f9a538ae0d70938d028
mudathirmahgoub <mudathirmahgoub@gmail.com>
2025-11-22 12:41:59 -0600

    more

--- linear_set_test.py
+++ linear_set_test.py
@@ -9,7 +9,25 @@ def phi(x, y):
 
 class TestLinearSet(unittest.TestCase):    
 
-    def test_formula(self):
+    def test_formula_x_y_geq_0(self):
+        A = toMacro([x == x])
+        B = toMacro([x >= 0, y >= 0])
+        A.args = set_vars + [a for a in A.args if a not in set_vars]
+        B.args = set_vars + [b for b in B.args if b not in set_vars]
+        sls = semilinear.SLS(B, set_vars, len(B.args))
+        sls.reduce()
+        while sls.augment():
+            sls.reduce()
+            print(sls.getSLS())
+            continue
+        
+        X = findSolution(A, sls)
+        print(f"solution: {X}")
+        if X: 
+            result = returnSolution(list(zip(A.args, X)), sls)
+            print(f"result: {result}")
+
+    def test_formula_x_eq_1(self):
         A = toMacro([x == x])
         B = toMacro([x == 1, y >= 1])
         A.args = set_vars + [a for a in A.args if a not in set_vars]
