e17885885a89fb26b308e762bbe3d0ea3ab11d6f
mudathirmahgoub <mudathirmahgoub@gmail.com>
2026-02-15 16:40:04 -0600

    card benchmarks

--- run_cvc5.py
+++ run_cvc5.py
@@ -1,8 +1,9 @@
 import os
 import csv
 import subprocess
+import time
 
-dirs = ["benchmarks/bapa/arith/cvc5","benchmarks/bapa/card/cvc5"]
+dirs = ["benchmarks/bapa/arith/cvc5", "benchmarks/bapa/card/cvc5"]
 output_csv = "cvc5_arith.csv"
 
 with open(output_csv, "w", newline="", encoding="utf-8") as csvfile:
@@ -12,14 +13,16 @@ with open(output_csv, "w", newline="", encoding="utf-8") as csvfile:
         files = sorted(os.listdir(dir))
         for filename in files:
             path = os.path.join(dir, filename)
-
             if os.path.isfile(path):
                 print(f"{path}")
+                start = time.time()
                 result = subprocess.run(
                     ["/home/mudathir/all/cvc5/liastar/build/bin/cvc5", path],
                     capture_output=True,
-                    text=True
+                    text=True,
                 )
-
+                end = time.time()
                 content = result.stdout.strip()
-                writer.writerow([path, content])
+                duration = end - start
+                print(duration)
+                writer.writerow([path, content, duration])
