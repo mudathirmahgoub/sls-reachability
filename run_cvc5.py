e3dccc1e08f970ecafd64350355fb7fb5202b674
mudathirmahgoub <mudathirmahgoub@gmail.com>
2026-02-15 16:24:52 -0600

    more

--- run_cvc5.py
+++ run_cvc5.py
@@ -2,24 +2,24 @@ import os
 import csv
 import subprocess
 
-directory = "benchmarks/bapa/arith/cvc5"
+dirs = ["benchmarks/bapa/arith/cvc5","benchmarks/bapa/card/cvc5"]
 output_csv = "cvc5_arith.csv"
 
 with open(output_csv, "w", newline="", encoding="utf-8") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["filename", "result"])
+    for dir in dirs:
+        files = sorted(os.listdir(dir))
+        for filename in files:
+            path = os.path.join(dir, filename)
 
-    files = sorted(os.listdir(directory))
-    for filename in files:
-        path = os.path.join(directory, filename)
+            if os.path.isfile(path):
+                print(f"{path}")
+                result = subprocess.run(
+                    ["/home/mudathir/all/cvc5/liastar/build/bin/cvc5", path],
+                    capture_output=True,
+                    text=True
+                )
 
-        if os.path.isfile(path):
-            print(f"{path}")
-            result = subprocess.run(
-                ["/home/mudathir/all/cvc5/liastar/build/bin/cvc5", path],
-                capture_output=True,
-                text=True
-            )
-
-            content = result.stdout.strip()
-            writer.writerow([filename, content])
+                content = result.stdout.strip()
+                writer.writerow([path, content])
