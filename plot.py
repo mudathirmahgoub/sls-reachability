605d044b230835d4595fbc61cacd440e64f84397
mudathirmahgoub <mudathirmahgoub@gmail.com>
2026-03-24 11:44:00 -0500

    update results

--- plot.py
+++ plot.py
@@ -7,24 +7,28 @@ df = pd.read_csv("comparison_mapa.csv")
 
 # === Filter out timeouts ===
 df_sls = df[df["sls result"].str.strip() != "timeout"]
-df_cvc5 = df[df["cvc5 lia result"].str.strip() != "timeout"]
+df_cvc5_lia = df[df["cvc5 lia result"].str.strip() != "timeout"]
+df_normaliz = df[df["lia normaliz result"].str.strip() != "timeout"]
 
 # === Extract and sort durations ===
-cvc5 = df_cvc5["cvc5 lia duration"].astype(float).sort_values().tolist()
-sls  = df_sls["sls duration"].astype(float).sort_values().tolist()
+column_cvc5_lia = df_cvc5_lia["cvc5 lia duration"].astype(float).sort_values().tolist()
+column_sls  = df_sls["sls duration"].astype(float).sort_values().tolist()
+column_normaliz  = df_normaliz["lia normaliz duration"].astype(float).sort_values().tolist()
 
 # === Compute cumulative sums ===
-cvc5_cum = np.cumsum(cvc5)
-sls_cum  = np.cumsum(sls)
+cvc5_cum = np.cumsum(column_cvc5_lia)
+sls_cum  = np.cumsum(column_sls)
+normaliz_cum  = np.cumsum(column_normaliz)
 
-# === X-axis: number of solved instances ===
-z_cvc5 = list(range(1, len(cvc5_cum) + 1))
-z_sls  = list(range(1, len(sls_cum) + 1))
+cvc5_lia= list(range(1, len(cvc5_cum) + 1))
+sls  = list(range(1, len(sls_cum) + 1))
+normaliz  = list(range(1, len(normaliz_cum) + 1))
 
 # === Plot ===
 plt.figure(figsize=(10, 6))
-plt.plot(cvc5_cum, z_cvc5, label="cvc5 lia", linewidth=2)
-plt.plot(sls_cum, z_sls, label="sls",  linewidth=2)
+plt.plot(cvc5_cum, cvc5_lia, label="cvc5 lia", linewidth=2)
+plt.plot(sls_cum, sls, label="sls",  linewidth=2)
+plt.plot(normaliz_cum, normaliz, label="cvc5 lia normaliz",  linewidth=2)
 
 plt.xlabel("Cumulative time (s)")
 plt.ylabel("Number of solved instances")
