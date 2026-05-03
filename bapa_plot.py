18023571697ea71492abc4c1f20a036208839333
mudathirmahgoub <mudathirmahgoub@gmail.com>
2026-05-03 16:45:20 -0500

    more

--- bapa_plot.py
+++ bapa_plot.py
@@ -9,24 +9,32 @@ df = pd.read_csv("comparison_bapa.csv")
 df_sql_solver = df[df["sqlsolver result"].astype(str).str.strip() != "timeout"]
 df_sls = df[df["sls result"].astype(str).str.strip() != "timeout"]
 df_cvc5_lia = df[df["cvc5 lia result"].astype(str).str.strip() != "timeout"]
-df_normaliz = df[df["lia normaliz result"].astype(str).str.strip() != "timeout"]
+# df_normaliz = df[df["lia normaliz result"].astype(str).str.strip() != "timeout"]
+df_unfold5 = df[df["unfold5 result"].astype(str).str.strip() != "timeout"]
+df_no_interp = df[df["no_interp result"].astype(str).str.strip() != "timeout"]
 
 # === Extract and sort durations ===
-column_sql_solver = df_cvc5_lia["sqlsolver duration"].astype(float).sort_values().tolist()
+column_sql_solver = df_sql_solver["sqlsolver duration"].astype(float).sort_values().tolist()
 column_cvc5_lia = df_cvc5_lia["cvc5 lia duration"].astype(float).sort_values().tolist()
 column_sls = df_sls["sls duration"].astype(float).sort_values().tolist()
-column_normaliz = df_normaliz["lia normaliz duration"].astype(float).sort_values().tolist()
+# column_normaliz = df_normaliz["lia normaliz duration"].astype(float).sort_values().tolist()
+column_unfold5 = df_unfold5["unfold5 duration"].astype(float).sort_values().tolist()
+column_no_interp = df_no_interp["no_interp duration"].astype(float).sort_values().tolist()
 
 # === Compute cumulative sums ===
 sql_solver_cum = np.cumsum(column_sql_solver)
 cvc5_cum = np.cumsum(column_cvc5_lia)
 sls_cum = np.cumsum(column_sls)
-normaliz_cum = np.cumsum(column_normaliz)
+# normaliz_cum = np.cumsum(column_normaliz)
+unfold5_cum = np.cumsum(column_unfold5)
+no_interp_cum = np.cumsum(column_no_interp)
 
 sql_solver_x = list(range(1, len(sql_solver_cum) + 1))
 cvc5_x = list(range(1, len(cvc5_cum) + 1))
 sls_x = list(range(1, len(sls_cum) + 1))
-normaliz_x = list(range(1, len(normaliz_cum) + 1))
+# normaliz_x = list(range(1, len(normaliz_cum) + 1))
+unfold5_x = list(range(1, len(unfold5_cum) + 1))
+no_interp_x = list(range(1, len(no_interp_cum) + 1))
 
 # === Plot ===
 plt.figure(figsize=(10, 6))
@@ -34,6 +42,8 @@ plt.plot(sql_solver_cum, sql_solver_x, label="sqlsolver", linewidth=2)
 plt.plot(cvc5_cum, cvc5_x, label="cvc5 lia", linewidth=2)
 plt.plot(sls_cum, sls_x, label="sls", linewidth=2)
 # plt.plot(normaliz_cum, normaliz_x, label="normaliz", linewidth=2)
+plt.plot(unfold5_cum, unfold5_x, label="unfold5", linewidth=2)
+plt.plot(no_interp_cum, no_interp_x, label="no_interp", linewidth=2)
 
 plt.xlabel("Cumulative time (s)")
 plt.ylabel("Number of solved instances")
