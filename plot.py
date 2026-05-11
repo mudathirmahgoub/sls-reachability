f65cfb1b9b788ded32168c43882fa0058a31aea5
mudathirmahgoub <mudathirmahgoub@gmail.com>
2026-05-11 18:35:20 -0500

    changes

--- plot.py
+++ plot.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 def plot(source_file, output1, output2):   
     # === Load CSV ===
     df = pd.read_csv(source_file)
-
+   
     # === Filter out timeouts ===
     df_sql_solver = df[df["sqlsolver result"].astype(str).str.strip() != "timeout"]
+    df_modified_sql_solver = df[(df["modified_sqlsolver result"].astype(str).str.strip() != "timeout") &
+                                   (df["modified_sqlsolver result"].astype(str).str.strip() != "unknown")]
     df_sls = df[df["sls result"].astype(str).str.strip() != "timeout"]
     df_cvc5_lia = df[df["cvc5 lia result"].astype(str).str.strip() != "timeout"]
     # df_normaliz = df[df["lia normaliz result"].astype(str).str.strip() != "timeout"]
@@ -16,6 +18,7 @@ def plot(source_file, output1, output2):
 
     # === Extract and sort durations ===
     column_sql_solver = df_sql_solver["sqlsolver duration"].astype(float).sort_values().tolist()
+    column__modified_sql_solver = df_modified_sql_solver["modified_sqlsolver duration"].astype(float).sort_values().tolist()
     column_cvc5_lia = df_cvc5_lia["cvc5 lia duration"].astype(float).sort_values().tolist()
     column_sls = df_sls["sls duration"].astype(float).sort_values().tolist()
     # column_normaliz = df_normaliz["lia normaliz duration"].astype(float).sort_values().tolist()
@@ -24,6 +27,7 @@ def plot(source_file, output1, output2):
 
     # === Compute cumulative sums ===
     sql_solver_cum = np.cumsum(column_sql_solver)
+    modified_sql_solver_cum = np.cumsum(column__modified_sql_solver)
     cvc5_cum = np.cumsum(column_cvc5_lia)
     sls_cum = np.cumsum(column_sls)
     # normaliz_cum = np.cumsum(column_normaliz)
@@ -31,6 +35,7 @@ def plot(source_file, output1, output2):
     no_interp_cum = np.cumsum(column_no_interp)
 
     sql_solver_x = list(range(1, len(sql_solver_cum) + 1))
+    modified_sql_solver_x = list(range(1, len(modified_sql_solver_cum) + 1))
     cvc5_x = list(range(1, len(cvc5_cum) + 1))
     sls_x = list(range(1, len(sls_cum) + 1))
     # normaliz_x = list(range(1, len(normaliz_cum) + 1))
@@ -40,6 +45,7 @@ def plot(source_file, output1, output2):
     # === Plot ===
     plt.figure(figsize=(10, 6))
     plt.plot(sql_solver_cum, sql_solver_x, label="SQLSolver", linewidth=2)
+    plt.plot(modified_sql_solver_cum, modified_sql_solver_x, label="Modified SQLSolver", linewidth=2)
     plt.plot(cvc5_cum, cvc5_x, label="cvc5", linewidth=2)
     plt.plot(sls_cum, sls_x, label="SLS-reachability (unfold-0)", linewidth=2)
     # plt.plot(normaliz_cum, normaliz_x, label="normaliz", linewidth=2)
